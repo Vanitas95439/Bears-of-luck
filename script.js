95c96131cedb03acde0fcb742da2cc6d1bf8b9fd
Vanitas <121865189+Vanitas95439@users.noreply.github.com>
2025-05-22 20:30:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+window.addEventListener("scroll", () => {
+  const elements = document.querySelectorAll(".scroll-item");
+  const centerY = window.innerHeight / 2;
+
+  elements.forEach((el) => {
+    const rect = el.getBoundingClientRect();
+    const distance = Math.abs(rect.top + rect.height / 2 - centerY);
+
+    const maxScale = 1.5;
+    const minScale = 0.7;
+    const scale = Math.max(
+      minScale,
+      maxScale - (distance / centerY) * (maxScale - minScale)
+    );
+
+    el.style.transform = `scale(${scale})`;
+    el.style.opacity = scale.toFixed(2);
+  });
+});
